f14f9a5e401d841c148882e47ce37373adfb169f
Joris Kok <joris_kok1@hotmail.com>
2022-07-19 20:14:34 +0200

    Add membership and schedule type

--- src/components/Articles/Article.js
+++ src/components/Articles/Article.js
@@ -47,16 +47,16 @@ function NewsLeftAndImageRight({ title, body, image }) {
 function NewsRightAndImageLeft({ title, body, image }) {
   return (
     <div className={"flex flex-row w-full gap-4"}>
-      <div className={"w-1/2 p-12"}>
-        <h2 className={"text-black"}>{title}</h2>
-        <p className={"text-gray-500 whitespace-pre-line"}>{body}</p>
-      </div>
       <div className={"w-1/2 flex flex-col justify-center p-16"}>
         <img
           src={image}
           alt={title}
           className={"w-full max-h-full object-contain"}
         />
+      </div>
+      <div className={"w-1/2 p-12"}>
+        <h2 className={"text-black"}>{title}</h2>
+        <p className={"text-gray-500 whitespace-pre-line"}>{body}</p>
       </div>
     </div>
   );
