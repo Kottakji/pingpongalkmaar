06a33245bbc7de652ef8fd979b7eb833d50539a2
Joris Kok <joris_kok1@hotmail.com>
2022-08-01 19:31:15 +0200

    Fix dependencies

--- preact.config.js
+++ preact.config.js
@@ -1,22 +1,46 @@
-const netlifyPlugin = require('preact-cli-plugin-netlify');
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const tailwind = require('preact-cli-tailwind');
+const netlifyPlugin = require("preact-cli-plugin-netlify");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const imageminMozjpeg = require("imagemin-mozjpeg");
 
 module.exports = (config, env, helpers) => {
-	netlifyPlugin(config);
-  config = tailwind(config, env, helpers);
-	env.production && !env.ssr && config.plugins.push(new ImageminPlugin({
-		from: './build/assets/**',
-		pngquant: {
-			quality: '60'
-		},
-		plugins: [
-			imageminMozjpeg({
-				quality: 50,
-				progressive: true
-			})
-		]
-	}));
-	return config;
+  netlifyPlugin(config);
+
+  const purgecss = require("@fullhuman/postcss-purgecss")({
+    // Specify the paths to all of the template files in your project
+    content: ["./src/**/*.js"],
+
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: (content) => content.match(params.regex) || [],
+  });
+
+  const postCssLoaders = helpers.getLoadersByName(config, "postcss-loader");
+  postCssLoaders.forEach(({ loader }) => {
+    const plugins = loader.options.postcssOptions.plugins;
+
+    // Add tailwind css at the top.
+    plugins.unshift(require("tailwindcss"));
+
+    // Add PurgeCSS only in production.
+    if (env.production) {
+      plugins.push(purgecss);
+    }
+  });
+
+  env.production &&
+    !env.ssr &&
+    config.plugins.push(
+      new ImageminPlugin({
+        from: "./build/assets/**",
+        pngquant: {
+          quality: "60",
+        },
+        plugins: [
+          imageminMozjpeg({
+            quality: 50,
+            progressive: true,
+          }),
+        ],
+      })
+    );
+  return config;
 };
