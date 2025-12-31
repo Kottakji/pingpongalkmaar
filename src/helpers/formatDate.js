46e0554671c13c70c2951e3aaea3fd4741789bfe
Joris Kok <joris_kok1@hotmail.com>
2025-12-31 11:32:00 +0100

    Fix build

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -1,9 +1,15 @@
 export default function formatDate(isoDate, format = "nl-NL") {
-  const date = new Date(isoDate);
+  if (!isoDate) return '';
 
-  return new Intl.DateTimeFormat(format, {
-    month: "2-digit",
-    day: "2-digit",
-    year: "numeric"
-  }).format(date);
+  try {
+    const date = new Date(isoDate);
+
+    return new Intl.DateTimeFormat(format, {
+      month: "2-digit",
+      day: "2-digit",
+      year: "numeric"
+    }).format(date);
+  } catch (e) {
+    return isoDate;
+  }
 }
