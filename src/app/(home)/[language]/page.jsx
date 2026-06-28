79a3981dfbebb71e3cc63a01218e8e9d12a876fb
Joris Kok <joris_kok1@hotmail.com>
2026-06-28 19:43:02 +0200

    New news on the website
    Signed-off-by: Joris Kok <joris_kok1@hotmail.com>

--- src/app/(home)/[language]/page.jsx
+++ src/app/(home)/[language]/page.jsx
@@ -1,15 +1,7 @@
 import Home from "@/components/Pages/Home";
-import { getInstagramPosts} from "@/api/instagram";
-import Instagram from "@/components/Instagram";
-
-export const revalidate = 3600; // Every hour
 
 export default async function Page({ params }) {
   const { language } = await params;
-  const posts = await getInstagramPosts();
 
-  return (<Home language={language}>
-      <Instagram language={language} posts={posts}/>
-    </Home>
-  );
+  return <Home language={language} />;
 }
--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,15 +1,5 @@
 import Home from "@/components/Pages/Home";
-import { getInstagramPosts} from "@/api/instagram";
-import Instagram from "@/components/Instagram";
 
-export const revalidate = 3600; // Every hour
-
-export default async function Page() {
-  const posts = await getInstagramPosts();
-
-  return (
-    <Home language={"nl"}>
-      <Instagram language={"nl"} posts={posts}/>
-    </Home>
-  );
+export default function Page() {
+  return <Home language={"nl"} />;
 };
