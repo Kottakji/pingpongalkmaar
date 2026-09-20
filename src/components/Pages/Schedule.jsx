068280a0aa5b94ccb45ee71d7b951d67a89f4f92
Joris Kok <joris_kok1@hotmail.com>
2026-09-20 11:33:02 +0200

    Update schedule
    Signed-off-by: Joris Kok <joris_kok1@hotmail.com>

--- src/components/Pages/Schedule.jsx
+++ src/components/Pages/Schedule.jsx
@@ -1,24 +1,11 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import Block from "@/components/Block";
 import useTranslation from "@/hooks/useTranslation";
 
-// The new season schedule takes effect on this date. Before then, visitors see
-// the current schedule plus an announcement; from this date on, the new one.
-const NEW_SCHEDULE_START = new Date("2026-08-17T00:00:00");
-
 export default function Schedule() {
   const { t } = useTranslation();
-  const [showNewSchedule, setShowNewSchedule] = useState(false);
-
-  useEffect(() => {
-    setShowNewSchedule(new Date() >= NEW_SCHEDULE_START);
-  }, []);
-
-  const blocks = showNewSchedule
-    ? t("schedule.blocksNew")
-    : [t("schedule.announcement"), ...t("schedule.blocksOld")];
+  const blocks = t("schedule.blocks");
 
   return (
     <main className="bg-[#121316] py-24">
